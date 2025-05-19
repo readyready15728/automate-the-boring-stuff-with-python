f096cb9cfd9f6e9ae2e93eee81b3e372a872ba52
Lynn Bradshaw <readyready15728@gmail.com>
2025-05-19 09:51:30 -0400

    Adding all_my_cats_1.py

---
+++ 04/all_my_cats_1.py
@@ -0,0 +1,26 @@
+print("Enter the name of cat 1:")
+cat_name_1 = input()
+print("Enter the name of cat 2:")
+cat_name_2 = input()
+print("Enter the name of cat 3:")
+cat_name_3 = input()
+print("Enter the name of cat 4:")
+cat_name_4 = input()
+print("Enter the name of cat 5:")
+cat_name_5 = input()
+print("Enter the name of cat 6:")
+cat_name_6 = input()
+print("The cat names are:")
+print(
+    cat_name_1
+    + " "
+    + cat_name_2
+    + " "
+    + cat_name_3
+    + " "
+    + cat_name_4
+    + " "
+    + cat_name_5
+    + " "
+    + cat_name_6
+)
