864d04e8664ddf1e44f0c2cb92cd7251b4eb3d26
Lynn Bradshaw <readyready15728@gmail.com>
2025-05-18 20:50:55 -0400

    Adding collatz_sequence.py

---
+++ 03/collatz_sequence.py
@@ -0,0 +1,18 @@
+def collatz(x):
+    while x != 1:
+        if x % 2 == 0:  # x is even
+            x /= 2
+        else:  # x is odd
+            x = 3 * x + 1
+
+        x = int(x)  # Conversion necessary due to division operation
+        yield x
+
+    # yield x  # The terminal 1 counts as part of the sequence
+
+
+print("Enter number: ", end="")
+seed = int(input())
+
+for x in collatz(seed):
+    print(x)
