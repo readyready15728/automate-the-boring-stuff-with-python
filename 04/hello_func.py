df49b429fe8cdbbbbcccfb83780532f07555ac87
Lynn Bradshaw <readyready15728@gmail.com>
2025-07-31 06:17:01 -0400

    Adding hello_func.py

---
+++ 04/hello_func.py
@@ -0,0 +1,11 @@
+def hello():
+    # Prints three greetings
+    print("Good morning!")
+    print("Good afternoon!")
+    print("Good evening!")
+
+
+hello()
+hello()
+print("ONE MORE TIME!")
+hello()
