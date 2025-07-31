52f45d6309407682cfb692579465e8152cf91b12
Lynn Bradshaw <readyready15728@gmail.com>
2025-07-30 23:43:05 -0400

    Adding swordfish.py

---
+++ 02/swordfish.py
@@ -0,0 +1,10 @@
+username = "Mary"
+password = "swordfish"
+
+if username == "Mary":
+    print("Hello, Mary")
+
+    if password == "swordfish":
+        print("Access granted.")
+    else:
+        print("Wrong password.")
