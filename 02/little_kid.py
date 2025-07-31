a1d82def690c60b6cde8c54206653897403f9bf0
Lynn Bradshaw <readyready15728@gmail.com>
2025-07-30 23:48:50 -0400

    Adding little_kid.py

---
+++ 02/little_kid.py
@@ -0,0 +1,9 @@
+name = "Carol"
+age = 3000
+
+if name == "Alice":
+    print("Hi, Alice.")
+elif age < 12:
+    print("Your are not Alice, kiddo.")
+else:
+    print("You are neither Alice nor a little kid.")
