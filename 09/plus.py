596208676e7436ef95593c4724870fad7f284cd8
Lynn Bradshaw <readyready15728@gmail.com>
2025-08-03 08:30:10 -0400

    Adding plus.py

---
+++ 09/plus.py
@@ -0,0 +1,6 @@
+import re
+
+pattern = re.compile(r"Eggs( and spam)+")
+print(pattern.search("Eggs and spam"))
+print(pattern.search("Eggs and spam and spam"))
+print(pattern.search("Eggs and spam and spam and spam"))
