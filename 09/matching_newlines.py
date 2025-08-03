752c5b6a79f87a6b0e02c1b81db16f98dfec4bec
Lynn Bradshaw <readyready15728@gmail.com>
2025-08-03 12:58:29 -0400

    Altering matching_newlines.py

--- 09/matching_newlines.py
+++ 09/matching_newlines.py
@@ -4,9 +4,11 @@ no_newline_re = re.compile(r".*")
 print(
     no_newline_re.search(
         "Serve the public trust.\nProtect the innocent.\nUphold the law."
-    )
+    ).group()
 )
 newline_re = re.compile(r".*", re.DOTALL)
 print(
-    newline_re.search("Serve the public trust.\nProtect the innocent.\nUphold the law.")
+    newline_re.search(
+        "Serve the public trust.\nProtect the innocent.\nUphold the law."
+    ).group()
 )
